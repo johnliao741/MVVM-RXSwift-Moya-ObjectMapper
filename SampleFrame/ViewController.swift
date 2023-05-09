b57c30a355f1adfb9192f69fbe6ef4ea181654bb
johnLiao <johnliao741@gmail.com>
2023-05-09 16:22:59 +0800

    Initial Commit

---
+++ SampleFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleFrame
+//
+//  Created by John on 2023/5/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
